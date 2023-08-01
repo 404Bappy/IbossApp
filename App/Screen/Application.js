0a040ab36250f3fb316c27d24399ab61c619f2a0
Bappy71 <19103076@iubat.edu>
2023-08-01 15:38:18 +0600

    I added text to the README file

--- App/Screen/Application.js
+++ App/Screen/Application.js
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import Header from "../Components/Home/Header";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Application() {
   return (
@@ -9,6 +10,60 @@ export default function Application() {
       <View>
         <Text style={styles.appText}>Applications</Text>
       </View>
+      <View style={styles.iconContainer}>
+        <View style={styles.firstLine}>
+          <View style={styles.container}>
+            <View style={styles.circle}>
+              <Ionicons name="train" size={50} color="white" />
+            </View>
+            <Text> Leave</Text>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.circle}>
+              <Ionicons name="car" size={50} color="white" />
+            </View>
+            <Text>Movement</Text>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.circle}>
+              <Ionicons name="train" size={50} color="white" />
+            </View>
+            <Text>Remote Att</Text>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.circle}>
+              <Ionicons name="train" size={50} color="white" />
+            </View>
+            <Text>Market Visit</Text>
+          </View>
+        </View>
+        <View style={styles.secondLine}>
+          <View style={styles.container}>
+            <View style={styles.circle}>
+              <Ionicons name="train" size={50} color="white" />
+            </View>
+            <Text> Leave</Text>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.circle}>
+              <Ionicons name="train" size={50} color="white" />
+            </View>
+            <Text>Movement</Text>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.circle}>
+              <Ionicons name="train" size={50} color="white" />
+            </View>
+            <Text>Remote Att</Text>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.circle}>
+              <Ionicons name="train" size={50} color="white" />
+            </View>
+            <Text>Market Visit</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,10 +71,28 @@ export default function Application() {
 const styles = StyleSheet.create({
   appText: {
     fontSize: 16,
-    fontWeight: 500,
     fontStyle: "normal",
+    fontWeight: "bold",
     marginTop: 16,
     marginLeft: 16,
+    lineHeight: 24,
     gap: 10,
+  },
+  circle: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#D444F1",
+  },
+  firstLine: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  secondLine: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 20,
   },
 });
