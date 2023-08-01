73b653ee2806cf77199aaddfb58e0569c5de0796
Bappy71 <19103076@iubat.edu>
2023-08-01 16:36:12 +0600

    I added text to the README file

--- App/Screen/Application.js
+++ App/Screen/Application.js
@@ -2,6 +2,8 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import Header from "../Components/Home/Header";
 import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function Application() {
   return (
@@ -13,54 +15,126 @@ export default function Application() {
       <View style={styles.iconContainer}>
         <View style={styles.firstLine}>
           <View style={styles.container}>
-            <View style={styles.circle}>
-              <Ionicons name="train" size={50} color="white" />
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 50,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#D444F1",
+              }}
+            >
+              <Ionicons name="train" size={20} color="white" />
             </View>
             <Text> Leave</Text>
           </View>
           <View style={styles.container}>
-            <View style={styles.circle}>
-              <Ionicons name="car" size={50} color="white" />
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 50,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#0BA5EC",
+              }}
+            >
+              <Ionicons name="car" size={20} color="white" />
             </View>
             <Text>Movement</Text>
           </View>
           <View style={styles.container}>
-            <View style={styles.circle}>
-              <Ionicons name="train" size={50} color="white" />
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 50,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#F63D68",
+              }}
+            >
+              <Ionicons name="location" size={20} color="white" />
             </View>
-            <Text>Remote Att</Text>
+            <Text style={{ textAlign: "center" }}>Remote Att</Text>
           </View>
           <View style={styles.container}>
-            <View style={styles.circle}>
-              <Ionicons name="train" size={50} color="white" />
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 50,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#669F2A",
+              }}
+            >
+              <Ionicons name="walk" size={20} color="white" />
             </View>
             <Text>Market Visit</Text>
           </View>
         </View>
         <View style={styles.secondLine}>
           <View style={styles.container}>
-            <View style={styles.circle}>
-              <Ionicons name="train" size={50} color="white" />
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 50,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#4E5BA6",
+              }}
+            >
+              <MaterialIcons name="perm-contact-cal" size={20} color="white" />
             </View>
-            <Text> Leave</Text>
+            <Text> att. Adjust</Text>
           </View>
           <View style={styles.container}>
-            <View style={styles.circle}>
-              <Ionicons name="train" size={50} color="white" />
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 40,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#15B79E",
+              }}
+            >
+              <FontAwesome name="map-o" size={20} color="white" />
             </View>
-            <Text>Movement</Text>
+            <Text>e-Presence</Text>
           </View>
           <View style={styles.container}>
-            <View style={styles.circle}>
-              <Ionicons name="train" size={50} color="white" />
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 50,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#EAAA08",
+              }}
+            >
+              <Ionicons name="logo-usd" size={20} color="white" />
             </View>
-            <Text>Remote Att</Text>
+            <Text> IOU</Text>
           </View>
           <View style={styles.container}>
-            <View style={styles.circle}>
-              <Ionicons name="train" size={50} color="white" />
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 50,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#344054",
+              }}
+            >
+              <Ionicons name="train" size={20} color="white" />
             </View>
-            <Text>Market Visit</Text>
+            <Text> Loan</Text>
           </View>
         </View>
       </View>
@@ -79,14 +153,15 @@ const styles = StyleSheet.create({
     gap: 10,
   },
   circle: {
-    width: 70,
-    height: 70,
+    width: 40,
+    height: 40,
     borderRadius: 50,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#D444F1",
   },
   firstLine: {
+      marginLeft:20,
     flexDirection: "row",
     justifyContent: "space-evenly",
   },
@@ -94,5 +169,12 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-evenly",
     marginTop: 20,
+  },
+  iconContainer: {
+    marginTop: 20,
+  },
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
