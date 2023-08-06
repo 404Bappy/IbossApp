6393da94f0ac1adfaf269c990b7b29001834a709
Bappy71 <19103076@iubat.edu>
2023-08-06 17:39:56 +0600

    first commit

--- App/Components/Culture-Components/Recognition.js
+++ App/Components/Culture-Components/Recognition.js
@@ -2,49 +2,55 @@ import { View, Text, StyleSheet, Image } from "react-native";
 import { Avatar, Card, IconButton } from "react-native-paper";
 import React from "react";
 import Header from "../Home/Header";
-import LinearGradient from "react-native-linear-gradient";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Recognition() {
   return (
     <View>
       <Header />
 
-      <View>
-        <LinearGradient
-          colors={["#085121", "#22984B"]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 0 }}
-          style={styles.gradientContainer}
+      <View style={styles.outerDiv}>
+        <View
+          style={{
+            marginTop: 20,
+          }}
         >
-          <View style={styles.innerDiv}>
-            <Card.Title
-              title="Card Title"
-              subtitle="Card Subtitle"
-              left={(props) => (
-                <Avatar.Icon
-                  {...props}
-                  size={24}
-                  icon="star"
-                  style={{
-                    backgroundColor: "#fff",
-                    color: "#085121",
-                    marginBottom: 10,
-                  }}
-                />
-              )}
-              right={(props) => (
-                <IconButton
-                  style={{}}
-                  {...props}
-                  icon="trophy"
-                  iconStyle={{ color: "white" }}
-                  size={45}
-                  onPress={() => {}}
-                />
-              )}
-            />
-          </View>
-        </LinearGradient>
+          <LinearGradient
+            colors={["#085121", "#22984B"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            style={styles.gradientContainer}
+          >
+            <View style={styles.innerDiv}>
+              <Card.Title
+                title="10200"
+                subtitle="Card Subtitle"
+                left={(props) => (
+                  <Avatar.Icon
+                    {...props}
+                    size={24}
+                    icon="star"
+                    style={{
+                      backgroundColor: "#fff",
+                      color: "#085121",
+                      marginBottom: 18,
+                    }}
+                  />
+                )}
+                right={(props) => (
+                  <IconButton
+                    style={{}}
+                    {...props}
+                    icon="trophy"
+                    iconStyle={{ color: "white" }}
+                    size={45}
+                    onPress={() => {}}
+                  />
+                )}
+              />
+            </View>
+          </LinearGradient>
+        </View>
       </View>
     </View>
   );
@@ -55,11 +61,11 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     overflow: "hidden",
   },
+
   innerDiv: {
     height: 76,
     width: 355,
     marginLeft: 20,
     borderRadius: 5,
-    marginTop: 20,
   },
 });
