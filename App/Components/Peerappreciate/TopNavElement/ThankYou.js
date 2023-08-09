9fbd2b557cc8e033e45275d7c83552e9f9c7f6a2
Bappy71 <19103076@iubat.edu>
2023-08-09 23:43:19 +0600

    first commit

---
+++ App/Components/Peerappreciate/TopNavElement/ThankYou.js
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function ThankYou() {
+  return (
+    <View>
+      <Text>ThankYou</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+});
